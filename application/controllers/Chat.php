4912cb6402ad385e557c1a7d3bc7dc55933a1694
Herman BUATON <hermanbuaton@gmail.com>
2017-02-03 16:39:24 +0800

    gitignore

--- application/controllers/Chat.php
+++ application/controllers/Chat.php
@@ -3,27 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Chat extends CI_Controller {
 
-    /**
-    * Index Page for this controller.
-    *
-    * Maps to the following URL
-    *    http://example.com/index.php/welcome
-    *  - or -
-    *    http://example.com/index.php/welcome/index
-    *  - or -
-    * Since this controller is set as the default controller in
-    * config/routes.php, it's displayed at http://example.com/
-    *
-    * So any other public methods not prefixed with an underscore will
-    * map to /index.php/welcome/<method_name>
-    * @see http://codeigniter.com/user_guide/general/urls.html
-    */
     public function index($room='')
     {
-        //check if logined already
-        if(!$this->session->userdata('user_name')) {
-            redirect('');
-        }
+        // validation
+        $this->checkRoom($room);
+        $this->checkLogin();
 
         $data['room'] = $room;
 
@@ -35,6 +19,25 @@ class Chat extends CI_Controller {
         $this->load->view('view_chat/view_chat_sidebar');
         $this->load->view('view_chat/view_chat_front');
         $this->load->view('view_chat/view_chat_footer');
+    }
+    
+    private function checkRoom($room)
+    {
+        if($room=='' || $room === null) {
+            redirect();
+        }
+        
+        return true;
+    }
+    
+    private function checkLogin()
+    {
+        //check if logined already
+        if(!$this->session->userdata('user_name')) {
+            redirect();
+        }
+        
+        return true;
     }
 
 }
