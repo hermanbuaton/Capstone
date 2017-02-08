4d3d2e40a34163c138cb0476082ce204e06cdc82
Herman BUATON <hermanbuaton@gmail.com>
2017-02-08 19:57:44 +0800

    testing

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -10,6 +10,8 @@ class User_model extends CI_Model {
     
     public function record_signin($data)
     {
+        $this->db->error();
+        
         $this->db->insert('user_log', $data);
         
         $id = $this->db->insert_id();
